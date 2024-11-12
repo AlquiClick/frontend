c94d90049360e7ba743b71fc0104cb428da94ccd
Matias Gomez <matiias.gomeez999@gmail.com>
2024-11-12 19:52:57 -0300

    fixe

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HeaderContainer from './components/header/HeaderContainer';
 import LoginForm from './components/LoginForm';
+import LoginV2Form from './components/LoginV2Form';
 import RegisterForm from './components/users/RegisterForm';
 import HomeContainer from './components/home/HomeContainer';
 import ProfileContainer from './components/profile/ProfileContainer';
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomeContainer />} />
           <Route path="/login" element={<LoginForm />} />
+          <Route path="/loginv2" element={<LoginV2Form />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/profile" element={<ProfileContainer />} />
           <Route path="/property" element={<PropertyContainer />} />
